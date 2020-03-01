88b6eee6f8dc360a53859155849a71a3c9406556
Joan Francesc Pedro Garcia <jfpedrog2000@gmail.com>
2020-03-02 00:39:35 +0100

    s'ha afegit els enunciats dels apartats 3.3 i 3.4

--- vim.c
+++ vim.c
@@ -140,6 +140,90 @@ void deleteCharOnMatrix(int files, int columnes, char matrix[files][columnes], i
 }
 
 /*
+		3.3.1-Establecer la ventana activa
+*/
+void setActiveWindow(int appData[], int aw) {
+	
+}
+
+/*
+		3.3.2-Inicializar una ventana (queda vacía)
+*/
+void iniWindow(int appData[], char Windows[][appData[1]][appData[2]], int Window) {
+	
+}
+
+/*
+		3.3.3-Inicializar todas las ventanas (todas quedan vacías)
+*/
+void iniWindows(int appData[], char windows[][appData[1]][appData[2]], int appCursors[][2]) {
+		
+}
+
+/*
+		3.3.4-Imprimir información de la aplicación
+*/
+void printWindowInfo(int appData[], int appCursors[][2]) {
+		
+}
+
+/*
+		3.3.5-Imprimir una pantalla por consola
+*/
+void printWindow(int appData[], char matrix[appData[1]][appData[2]]) {
+		
+}
+
+/*
+		3.3.6-Imprimir la pantalla activa de la aplicación
+*/
+void printCurrentWindow(int appData[], char windows[][appData[1]][appData[2]], int appCursors[][2]) {
+		
+}
+
+/*
+		3.3.7-Comprobar que la fila es legal en la aplicación
+*/
+bool isLegalFile(int appData[], int y) {
+		
+}
+
+/*
+		3.3.8-Establecer la fila del cursor en la ventana activa
+*/
+void setYCursorOnWindow(int appData[], int appCursors[][2], int y) {
+		
+}
+
+/*
+		3.3.9-Comprobar que la columna es legal en la aplicación
+*/
+bool isLegalColumn(int appData[], int x) {
+		
+}
+
+/*
+		3.3.10-Establecer la columna del cursor en la ventana activa
+*/
+void setXCursorOnWindow(int appData[], int appCursors[][2], int x) {
+		
+}
+
+/*
+		3.4.1-Insertar un carácter en la posición actual en la ventana activa
+*/
+void insertCharOnWindow(int appData[], char windows[][appData[1]][appData[2]], int appCursors[][2], char c) {
+		
+}
+
+/*
+		3.4.2-Borrar el carácter de la posición actual en la ventana activa
+*/
+void deleteCurrentPositionOnWindow(int appData[], char windows[][appData[1]][appData[2]], int appCursors[][2]) {
+		
+}
+
+/*
 		Función principal
 */
 int main(){
